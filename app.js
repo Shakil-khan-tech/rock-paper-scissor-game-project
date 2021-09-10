5e87ca0bfb2e313e8a838e958c6e83c7b85ebc80
Md Shakil Khan <mdshakilkhan706@gmail.com>
2021-09-11 04:28:25 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,83 +1,356 @@
-var   computerscore = 0;
-var   userscore = 0;
-const userscore_span = document.getElementById("user-score");
-const computerscore_span = document.getElementById("computer-score");
-const scoreboard_div = document.querySelector(".score-board");
-const result_p = document.querySelector(".result >p");
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
-const scissor_div = document.getElementById("s");
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
 
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
 
-function win(userchoice,computerchoice) {
-	const smalluser = "user".fontsize(3).sub();
-	const smallcomp = "comp".fontsize(3).sub();
-	const userchoice_div = document.getElementById(userchoice).classList;
-	userscore++;
-	userscore_span.innerHTML =userscore;
-	result_p.innerHTML = `${convert(userchoice)}${smalluser} beats ${convert(computerchoice)}${smallcomp} => You win!`;
-	userchoice_div.add('green-glow');
-    setTimeout(() => userchoice_div.remove('green-glow'), 300);//instance of the function...
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+  
+  {
+  "particles": {
+    "number": {
+      "value": 40,
+      "density": {
+        "enable": false,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "image",
+      "stroke": {
+        "width": 100,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "Musical note icons-02.png",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 5,
+        "opacity_min": .5,
+        "sync": true
+      }
+    },
+    "size": {
+      "value": 15,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 10,
+        "size_min": 0.3,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": false,
+      "speed": 5,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "bounce",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 600,
+        "rotateY": 600
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 250,
+        "size": 0,
+        "duration": 2,
+        "opacity": 0,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 400,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
 }
 
-
-function lose(userchoice,computerchoice) {
-	const smalluser = "user".fontsize(3).sub();
-	const smallcomp = "comp".fontsize(3).sub();
-	const userchoice_div = document.getElementById(userchoice).classList;
-    computerscore++;
-	computerscore_span.innerHTML =computerscore;
-	result_p.innerHTML = `${convert(userchoice)}${smalluser} loses ${convert(computerchoice)}${smallcomp} => You lost! `;
-	userchoice_div.add('red-glow');
-    setTimeout(() => userchoice_div.remove('red-glow'), 300);
+);
+particlesJS('particles1',
+  
+  {
+  "particles": {
+    "number": {
+      "value": 15,
+      "density": {
+        "enable": false,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "image",
+      "stroke": {
+        "width": 100,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "Musical note icons-06.png",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 5,
+        "opacity_min": .5,
+        "sync": true
+      }
+    },
+    "size": {
+      "value": 30,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 10,
+        "size_min": 0.3,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 5,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "bounce",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 600,
+        "rotateY": 600
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 250,
+        "size": 0,
+        "duration": 2,
+        "opacity": 0,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 400,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
 }
 
-function draw(userchoice,computerchoice) {
-	const smalluser = "user".fontsize(3).sub();
-	const smallcomp = "comp".fontsize(3).sub();
-	const userchoice_div = document.getElementById(userchoice).classList;
-	result_p.innerHTML = `${convert(userchoice)}${smalluser} equals ${convert(computerchoice)}${smallcomp} => DRAWWWW!`;
-	userchoice_div.add('gray-glow');
-    setTimeout(function(){ userchoice_div.remove('gray-glow') }, 300);
+);
+
+particlesJS('particles2',
+  
+  {
+  "particles": {
+    "number": {
+      "value": 15,
+      "density": {
+        "enable": false,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "image",
+      "stroke": {
+        "width": 100,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "Musical note icons-03.png",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 5,
+        "opacity_min": .5,
+        "sync": true
+      }
+    },
+    "size": {
+      "value": 30,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 10,
+        "size_min": 0.3,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 5,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "bounce",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 600,
+        "rotateY": 600
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 250,
+        "size": 0,
+        "duration": 2,
+        "opacity": 0,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 400,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
 }
 
-function convert(letter) {
-	if(letter==="r") return "Rock";
-	if(letter==="p") return "Paper";
-	if(letter==="s") return "Scissor";
-}
-
-function getcomputer() {
-const choices = ['r','p','s'];
-const randomnumber = Math.floor(Math.random() * 3);
-return choices[randomnumber];
-}
-
-function game(userchoice) {
-  const computerchoice = getcomputer();
-  switch(userchoice + computerchoice) {
-  	case "pr":
-  	case "sp":
-  	case "rs":
-  	win(userchoice,computerchoice);
-  	break;
-  	case "rp":
-  	case "ps":
-  	case "sr":
-  	lose(userchoice,computerchoice);
-  	break;
-  	case "pp":
-  	case "rr":
-  	case "ss":
-  	draw(userchoice,computerchoice);
-  	break;
-  }
-}
-
-function main() {
-	rock_div.addEventListener('click',() => game("r"));
-	paper_div.addEventListener('click',() => game("p"));
-	scissor_div.addEventListener('click',function() {game("s")});//instance of =>..
-}
-
-main();
+);
